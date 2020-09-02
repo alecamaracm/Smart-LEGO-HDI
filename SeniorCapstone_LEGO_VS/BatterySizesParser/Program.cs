7dd9376f7991bc849e0fd839572180578b2a9ae3
Alejandro Cabrerizo <alecamar@hotmail.es>
2020-09-02 09:21:29 -0700

    First CAD design pictures and copy of documentation docs

--- SeniorCapstone_LEGO_VS/BatterySizesParser/Program.cs
+++ SeniorCapstone_LEGO_VS/BatterySizesParser/Program.cs
@@ -15,20 +15,38 @@ namespace BatterySizesParser
         static void Main(string[] args)
         {
             System.Globalization.CultureInfo.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
-            string data = File.ReadAllText(Environment.CurrentDirectory + "/Data/11-15mm.txt");
 
-         
-            
-            var match = Regex.Match(data, "<tr class=\"row-(?:.[^><]*)\" role=\"row\"><td class=\"column-1\">(?:.[^><]*)</td><td class=\"column-2\">(?:.[^><]*)</td><td class=\"column-3\">(.[^><]*)</td><td class=\"column-4\">(?:.[^><]*)</td><td class=\"column-5\">(.[^><]*)</td><td class=\"column-6\">(.[^><]*)</td><td class=\"column-7\">(.[^><]*)</td><td class=\"column-8\">(.[^><]*)</td></tr>");
+            List<(string part, float capacity, float thickness, float width, float height)> batteryParsedData = new List<(string part, float capacity, float thickness, float width, float height)>();
 
-            List<(string part, float capacity, float thickness, float width, float height)> batteryParsedData=new List<(string part, float capacity, float thickness, float width, float height)>();
-
-            do
+            foreach (var file in Directory.GetFiles(Environment.CurrentDirectory + "/Data"))
             {
-                batteryParsedData.Add((match.Groups[1].Captures[0].Value, float.Parse(match.Groups[2].Captures[0].Value), float.Parse(match.Groups[3].Captures[0].Value), float.Parse(match.Groups[4].Captures[0].Value), float.Parse(match.Groups[5].Captures[0].Value)));
-                match = match.NextMatch();
-            } while (match.Success);
-        
+                string data = File.ReadAllText(file);
+                var match = Regex.Match(data, "<tr class=\"row-(?:.[^><]*)\" role=\"row\"><td class=\"column-1\">(?:.[^><]*)</td><td class=\"column-2\">(?:.[^><]*)</td><td class=\"column-3\">(.[^><]*)</td><td class=\"column-4\">(?:.[^><]*)</td><td class=\"column-5\">(.[^><]*)</td><td class=\"column-6\">(.[^><]*)</td><td class=\"column-7\">(.[^><]*)</td><td class=\"column-8\">(.[^><]*)</td></tr>");
+
+                do
+                {
+                    try
+                    {
+                        batteryParsedData.Add((match.Groups[1].Captures[0].Value, float.Parse(match.Groups[2].Captures[0].Value), float.Parse(match.Groups[3].Captures[0].Value.Split('~').Last()), float.Parse(match.Groups[4].Captures[0].Value), float.Parse(match.Groups[5].Captures[0].Value)));
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                     match = match.NextMatch();
+                } while (match.Success);
+            }
+
+            var thatFit4x2Duplo = batteryParsedData.Where(p => FitsRectangle(p, 48.5, 28.6) && p.thickness <= 4 && p.capacity>250).OrderByDescending(p=>p.capacity).ToList();
+            var thatFit2x2Duplo = batteryParsedData.Where(p => FitsRectangle(p, 16.6, 28.6) && p.thickness <= 4  && p.capacity > 50).OrderByDescending(p => p.capacity).ToList();
+
+            var thatFit4x2Normal = batteryParsedData.Where(p => FitsRectangle(p, 28, 12) && p.thickness <= 3  && p.capacity > 40).OrderByDescending(p => p.capacity).ToList();
+            var thatFit2x2Normal = batteryParsedData.Where(p => FitsRectangle(p, 12, 13.5) && p.thickness <= 3 && p.capacity > 15).OrderByDescending(p => p.capacity).ToList();
+        }
+
+        private static bool FitsRectangle((string part, float capacity, float thickness, float width, float height) part, double side1, double side2)
+        {
+            return (part.width<=side1 && part.height<=side2) || (part.width <= side2 && part.height <= side1);
         }
     }
 }
