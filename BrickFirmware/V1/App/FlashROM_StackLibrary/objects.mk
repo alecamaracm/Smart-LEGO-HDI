5f1981e7c64f9bbad61582d27be7526273b9858a
Alejandro Cabrerizo <alecamar@hotmail.es>
2020-10-05 00:23:40 -0400

    Cache files cleanup

--- BrickFirmware/V1/App/FlashROM_StackLibrary/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"C:/ti/simplelink_cc2640r2_sdk_4_20_00_04/source/ti/devices/cc26x0r2/driverlib/bin/ccs/driverlib.lib" -l"C:/ti/simplelink_cc2640r2_sdk_4_20_00_04/kernel/tirtos/packages/ti/dpl/lib/dpl_cc26x0r2.aem3" -l"C:/ti/simplelink_cc2640r2_sdk_4_20_00_04/source/ti/drivers/lib/drivers_cc26x0r2.aem3" -l"C:/ti/simplelink_cc2640r2_sdk_4_20_00_04/source/ti/drivers/rf/lib/rf_singleMode_cc26x0r2.aem3" -l"C:/ti/simplelink_cc2640r2_sdk_4_20_00_04/source/ti/display/lib/display.aem3" -l"C:/ti/simplelink_cc2640r2_sdk_4_20_00_04/source/ti/grlib/lib/ccs/m3/grlib.a" -l"F:/SeniorCapstone_LEGO/SeniorCapstone_LEGO/BrickFirmware/V1/SDK/FlashROM_Library/ble_r2.symbols" -l"F:/SeniorCapstone_LEGO/SeniorCapstone_LEGO/BrickFirmware/V1/SDK/FlashROM_Library/lib_linker.cmd" -l"F:/SeniorCapstone_LEGO/SeniorCapstone_LEGO/BrickFirmware/V1/SDK/FlashROM_Library/ble5_simple_peripheral_cc2640r2lp_stack_library.lib" -l"C:/ti/simplelink_cc2640r2_sdk_4_20_00_04/source/ti/ble5stack/common/cc26xx/ccs/cc26xx_app.cmd" -llibc.a
-
